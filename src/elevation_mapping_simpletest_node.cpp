6cdf39ae2244ff8b33ca30207b426c1ea3fcb361
trunc8 <sahasiddharth611@gmail.com>
2023-04-01 17:12:18 -0400

    Created elevation mapping object in simpletest node

--- src/elevation_mapping_simpletest_node.cpp
+++ src/elevation_mapping_simpletest_node.cpp
@@ -4,8 +4,14 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
     std::cout << "Hello elevation_mapping world!" << std::endl;
+
+    ros::init(argc, argv, "elevation_mapping_simpletest_node");
+    ros::NodeHandle nh;
+    elevation_mapping::ElevationMap map(nh);
+    
+
     return 0;
 }
